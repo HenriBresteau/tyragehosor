b2bdb5f50d02e80d4baa385b908cf0d59b0dcfb1
Henri Bresteau <henri.bresteau@gamil.com>
2022-11-15 16:44:31 +0100

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <img src="/src/assets/logo.svg" alt="logo" />
+      <h1>Tyragehösor</h1>
+    </div>
+  );
+}
+
+export default App;
