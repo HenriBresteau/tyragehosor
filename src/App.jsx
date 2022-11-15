7e8ad893ba200417eefe8c77f71459a1da134a71
Henri Bresteau <henri.bresteau@gamil.com>
2022-11-15 17:01:28 +0100

    add fonts and test it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <img src="/src/assets/logo.svg" alt="logo" />
       <h1>Tyragehösor</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        fugiat illum consequuntur voluptatem quos dolor quia accusamus facere
+        ducimus doloremque numquam amet exercitationem ipsum, praesentium
+        aliquam id repudiandae hic harum.
+      </p>
     </div>
   );
 }
