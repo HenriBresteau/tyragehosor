ee0cc32bb703900952c628ffe3f5996542e022ca
Henri Bresteau <henri.bresteau@gamil.com>
2022-11-17 10:28:06 +0100

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Link as NavLink,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
+import KiDonneAKi from "./Component/KiDonneAKi";
 import Navbar from "./Component/Navbar";
 import { Themes } from "./Component/Themes";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Themes />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Themes />}></Route>
+          <Route exact path="/KiDonneAKi" element={<KiDonneAKi />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
