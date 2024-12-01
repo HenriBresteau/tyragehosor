29849facd6373b94c04ebd3eea24c1028180eae1
Henri Bresteau <henri.bresteau@gmail.com>
2024-12-01 14:40:19 +0100

    [FIX] Tailwind fix

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 const Button = ({ handleDraw }) => {
-  return (
-    <div className="my-4 pt-12">
-      <button
-        className="flex items-center gap-2 bg-black border-2 border-white text-white py-4 px-6 rounded-md font-[GilroyB] tracking-widest text-xl transition-colors hover:text-slate-900 hover:bg-slate-100 "
-        onClick={handleDraw}
-      >
-        Tyragehösor
-        <img src="/cactus-1.svg" alt="cactus" className=""/>
-      </button>
-    </div>
-  );
+    return (
+        <div className="my-4 pt-12">
+            <button
+                className="flex items-center gap-2 bg-green text-white py-4 px-6 rounded-md font-gilroy font-bold uppercase tracking-widest text-xl transition-colors  hover:bg-greenlight "
+                onClick={handleDraw}
+            >
+                Tyragehösor
+            </button>
+        </div>
+    );
 };
 
 export default Button;
--- src/Component/Title.jsx
+++ src/Component/Title.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Title = ({ children }) => {
-  return <h2 className="text-2xl font-[GilroyB] underline mb-4">{children}</h2>;
+    return (
+        <h2 className="text-7xl font-gilroy font-bold mb-4 tracking-tight">
+            {children}
+        </h2>
+    );
 };
 
 export default Title;
