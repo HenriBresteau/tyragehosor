c50e8ad3c455c7988feb6c35f6c6e3823ba0e549
Henri Bresteau <henri.bresteau@gmail.com>
2024-12-01 14:39:36 +0100

    [FIX] Navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,66 +1,68 @@
-import { GiftIcon, LightBulbIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { Gift, Home, Lightbulb } from "lucide-react";
+import { Link, NavLink } from "react-router-dom";
 
 const menu = [
-  {
-    name: "KiDonneAKi",
-    href: "/Kidonneaki",
-    icon: GiftIcon,
-    disable: false
-  },{
-    name: "Thèmes",
-    href: "/Themes",
-    icon: LightBulbIcon,
-    disable: true
-  }
+    {
+        name: "Home",
+        href: "/",
+        icon: Home,
+    },
+    {
+        name: "Thèmes",
+        href: "/Themes",
+        icon: Lightbulb,
+    },
+    {
+        name: "KiDonneAKi",
+        href: "/Kidonneaki",
+        icon: Gift,
+    },
 ];
 
 export default function Navbar() {
-  return (
-    <div className="bg-white">
-      <nav className="flex items-center justify-between mx-auto max-w-4xl px-6 border-b-2 border-gray-100  py-6">
-        <Link to="/" className="flex items-center space-x-2">
-          <img
-            className="h-10 w-auto"
-            src="/cactus-2.svg"
-            alt="logo"
-          />
-          <h1 className="text-xl text-transparent bg-clip-text bg-gradient-to-r from-[#7C90A0] to-[#330867] ">
-            Tyragehösor
-          </h1>
-        </Link>
-        <ul className="flex space-x-10">
-          {menu.map((item) => (
-            <li key={item.name}>
-            {item.disable ? (
-              <div className="-m-3 flex items-center rounded-md p-3 cursor-not-allowed opacity-50">
-                <item.icon
-                  className="h-6 w-6 flex-shrink-0 text-[#7C90A0] "
-                  aria-hidden="true"
-                />
-                <span className="ml-3 text-base font-medium text-[#330867] ">
-                  {item.name}
-                </span>
-              </div>
-            ) : (
-              <Link
-                key={item.name}
-                to={item.href}
-                className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
-              >
-                <item.icon
-                  className="h-6 w-6 flex-shrink-0 text-[#7C90A0] "
-                  aria-hidden="true"
-                />
-                <span className="ml-3 text-base font-medium text-[#330867] ">
-                  {item.name}
-                </span>
-              </Link>
-            )}
-          </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
-  );
+    return (
+        <div className="bg-white border-b-2 border-gray-100">
+            <nav className="flex items-center justify-between mx-auto max-w-5xl py-6">
+                <Link to="/" className="flex items-center space-x-2">
+                    <img
+                        className="h-12 w-12"
+                        src="/t-rex-icon.png"
+                        alt="logo"
+                    />
+                    <h1 className="text-lg uppercase font-gilroy font-extrabold text-transparent bg-clip-text bg-gradient-to-l from-[#92d079] to-[#42762e] ">
+                        Tyragehösor
+                    </h1>
+                </Link>
+                <ul className="flex gap-6 bg-greenlight/10 px-6 py-2 rounded-xl">
+                    {menu.map((item) => (
+                        <li key={item.name}>
+                            <NavLink
+                                to={item.href}
+                                className={({ isActive }) =>
+                                    clsx(
+                                        "text-lg font-medium transition-colors flex items-center py-1 px-2 group hover:text-green ",
+                                        isActive
+                                            ? "text-green-500 border-b-2 border-green "
+                                            : "text-black"
+                                    )
+                                }
+                                aria-current={({ isActive }) =>
+                                    isActive ? "page" : undefined
+                                }
+                            >
+                                <item.icon
+                                    className="h-6 w-6 flex-shrink-0"
+                                    aria-hidden="true"
+                                />
+                                <span className="ml-3 font-gilroy  text-base font-semibold text-inherit group-hover:text-green">
+                                    {item.name}
+                                </span>
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
 }
